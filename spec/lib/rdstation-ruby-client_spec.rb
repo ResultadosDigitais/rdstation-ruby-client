af5f65a25c9c9ac27d6d87ec151c85aacdd260ac
Ary Rabelo <aryrabelo@gmail.com>
2015-01-29 18:31:23 +0000

    adding tests

--- spec/lib/rdstation-ruby-client_spec.rb
+++ spec/lib/rdstation-ruby-client_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
-describe PeriodicTable do
-  it "should return data for a named element" do
-    element_data = PeriodicTable.lookup('oxygen')
-    element_data.should_not be_nil
-    element_data.symbol.should == 'O'
-    element_data.atomic_weight.should == '15.9994'
+describe RDStation do
+    let(:rdstation_client) { RDStation::Client.new(ENV['RD_STATION_TOKEN'],
+                                                   ENV['RD_STATION_TOKEN_PRIVATE']) }
+    let(:lead_email) { "iginosms@gmail.com" }
+    let(:amount) { 300}
+
+
+  context "#change_lead_status" do
+    before do
+      VCR.use_cassette('change_lead_status') do
+         @response = rdstation_client.change_lead_status(email: lead_email,  status: 'won', value: amount)
+      end
+    end
+    it 'should see the vars' do
+      expect(lead_email).to_not be_nil
+      expect(amount).to be_a_kind_of(Numeric)
+    end
+    it 'work?' do
+      expect(@response.code).to eql(404)
+    end
+
   end
+
 end
