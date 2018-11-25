1cb4b7fb18c050f2ef153b8a81ab82cbddb33a77
Filipe Nascimento <filipemateusn@gmail.com>
2018-11-25 19:27:21 -0200

    Adapt default errors to the new structure

--- lib/rdstation/error_handler/default.rb
+++ lib/rdstation/error_handler/default.rb
@@ -1,18 +1,14 @@
 module RDStation
   class ErrorHandler
     class Default
-      attr_reader :api_response
-      EXCEPTION_CLASS = RDStation::Error::Default
+      attr_reader :errors
 
-      def initialize(api_response)
-        @api_response = api_response
+      def initialize(errors)
+        @errors = errors
       end
 
       def raise_error
-        raise RDStation::Error::Default.new(
-          'An unrecognized error has occurred.',
-          api_response
-        )
+        raise RDStation::Error::Default, errors
       end
     end
   end
