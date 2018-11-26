4873c156eb52e47625d24201a057146756138971
Filipe Nascimento <filipemateusn@gmail.com>
2018-11-26 10:29:52 -0200

    Adapt invalid credentials errors to the new structure

---
+++ spec/lib/rdstation/error_handler/invalid_credentials_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+RSpec.describe RDStation::ErrorHandler::InvalidCredentials do
+  describe '#raise_error' do
+
+    subject(:invalid_credentials_error) { described_class.new(errors) }
+
+    context 'when there is an expired code grant error' do
+      let(:errors) do
+        [
+          {
+            'error_message' => 'Error Message',
+            'error_type' => 'ACCESS_DENIED'
+          }
+        ]
+      end
+
+      it 'raises an InvalidCredentials error' do
+        expect do
+          invalid_credentials_error.raise_error
+        end.to raise_error(RDStation::Error::InvalidCredentials, 'Error Message')
+      end
+    end
+
+    context 'when none of the errors are expired code grant errors' do
+      let(:errors) do
+        [
+          {
+            'error_message' => 'Error Message',
+            'error_type' => 'RANDOM_ERROR_TYPE'
+          },
+          {
+            'error_message' => 'Another Error Message',
+            'error_type' => 'ANOTHER_RANDOM_ERROR_TYPE'
+          }
+        ]
+      end
+
+      it 'does not raise an InvalidCredentials error' do
+        result = invalid_credentials_error.raise_error
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when there are no errors' do
+      let(:errors) { [] }
+
+      it 'does not raise an InvalidCredentials error' do
+        result = invalid_credentials_error.raise_error
+        expect(result).to be_nil
+      end
+    end
+  end
+end
