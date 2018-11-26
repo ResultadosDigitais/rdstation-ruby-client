09994c620136151b122a35216fb425e2d7a56330
Filipe Nascimento <filipemateusn@gmail.com>
2018-11-26 10:21:13 -0200

    Adapt expired code grant errors to the new structure

--- lib/rdstation/error_handler/expired_code_grant.rb
+++ lib/rdstation/error_handler/expired_code_grant.rb
@@ -1,26 +1,23 @@
 module RDStation
   class ErrorHandler
     class ExpiredCodeGrant
-      attr_reader :api_response, :response_headers, :error
+      attr_reader :errors
 
       ERROR_CODE = 'EXPIRED_CODE_GRANT'.freeze
-      EXCEPTION_CLASS = RDStation::Error::ExpiredCodeGrant
 
-      def initialize(api_response)
-        @api_response = api_response
-        @error = JSON.parse(api_response.body)['errors']
-        @response_headers = api_response.headers
+      def initialize(errors)
+        @errors = errors
       end
 
       def raise_error
-        return unless expired_code?
-        raise EXCEPTION_CLASS.new(error['error_message'], api_response)
+        return if expired_code_errors.empty?
+        raise RDStation::Error::ExpiredCodeGrant, expired_code_errors.first
       end
 
       private
 
-      def expired_code?
-        error['error_type'] == ERROR_CODE
+      def expired_code_errors
+        errors.select { |error| error['error_type'] == ERROR_CODE }
       end
     end
   end
