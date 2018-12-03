0435f002616ba3d1c155093a8f5f4fdfd0ce7e0b
Filipe Nascimento <filipemateusn@gmail.com>
2018-12-03 20:15:46 -0200

    Post payload to the events API

--- lib/rdstation/events.rb
+++ lib/rdstation/events.rb
@@ -11,16 +11,7 @@ module RDStation
     end
 
     def create(payload)
-      # {
-      #   "event_type": "OPPORTUNITY_LOST",
-      #   "event_family":"CDP",
-      #   "payload": {
-      #     "email": "email@email.com",
-      #     "funnel_name": "default",
-      #     "reason":"Lost reason"
-      #   }
-      # }
-      response = self.class.post(EVENTS_ENDPOINT, headers: required_headers)
+      response = self.class.post(EVENTS_ENDPOINT, headers: required_headers, body: payload.to_json)
       response_body = JSON.parse(response.body)
       return response_body unless errors?(response_body)
       RDStation::ErrorHandler.new(response).raise_errors
