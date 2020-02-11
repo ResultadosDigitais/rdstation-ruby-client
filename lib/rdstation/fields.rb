45cd28c4fa568f7cdd0cd1ae4d8f925344e9d54f
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-02-11 13:33:30 -0300

    Add create option in fields

--- lib/rdstation/fields.rb
+++ lib/rdstation/fields.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 module RDStation
-  # More info: https://developers.rdstation.com/pt-BR/reference/contacts
+  # More info: https://developers.rdstation.com/pt-BR/reference/fields
   class Fields
     include HTTParty
     include ::RDStation::RetryableRequest
 
     BASE_URL = 'https://api.rd.services/platform/contacts/fields'.freeze
-    
+
     def initialize(authorization:)
       @authorization = authorization
     end
@@ -14,6 +14,13 @@ module RDStation
     def all
       retryable_request(@authorization) do |authorization|
         response = self.class.get(BASE_URL, headers: authorization.headers)
+        ApiResponse.build(response)
+      end
+      end
+
+    def create(payload)
+      retryable_request(@authorization) do |authorization|
+        response = self.class.post(BASE_URL, headers: authorization.headers, body: payload.to_json)
         ApiResponse.build(response)
       end
     end
