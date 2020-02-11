bb2e1ddf5dc4ab9abbc7710ce6e2f04dd7f4f8f5
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-02-11 13:28:57 -0300

    Update to use API Response module

--- lib/rdstation/events.rb
+++ lib/rdstation/events.rb
@@ -12,16 +12,8 @@ module RDStation
     def create(payload)
       retryable_request(@authorization) do |authorization|
         response = self.class.post(EVENTS_ENDPOINT, headers: authorization.headers, body: payload.to_json)
-        response_body = JSON.parse(response.body)
-        return response_body unless errors?(response_body)
-        RDStation::ErrorHandler.new(response).raise_error
+        ApiResponse.build(response)
       end
-    end
-
-    private
-
-    def errors?(response_body)
-      response_body.is_a?(Array) || response_body['errors']
     end
   end
 end
