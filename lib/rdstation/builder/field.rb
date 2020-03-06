e5ee0ed727aedf489d704c33704b7873e09b6b71
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-03-06 12:30:16 -0300

    Finish Field Builder and tests

--- lib/rdstation/builder/field.rb
+++ lib/rdstation/builder/field.rb
@@ -6,27 +6,30 @@ module RDStation
     class Field
       DATA_TYPES = %w(STRING INTEGER BOOLEAN STRING[]).freeze
       PRESENTATION_TYPES = %w[TEXT_INPUT TEXT_AREA URL_INPUT PHONE_INPUT
-                              EMAIL_INPUT CHECK_BOX NUMBER_INPUT COMBO_BOX RADIO_
-                              BUTTON MULTIPLE_CHOICE].freeze
+                              EMAIL_INPUT CHECK_BOX NUMBER_INPUT COMBO_BOX
+                              RADIO_BUTTON MULTIPLE_CHOICE].freeze
 
       REQUIRED_FIELDS = %w[api_identifier data_type presentation_type label name].freeze
 
       def initialize(api_identifier)
         raise 'api_identifier required' unless api_identifier
+        unless valid_identifier(api_identifier)
+          raise 'api_identifier is not in a valid format, need start with "cf_"'
+        end
 
         @values = {}
         @values['api_identifier'] = api_identifier
       end
 
       def data_type(data_type)
-        raise 'Not valid data_type' unless DATA_TYPES.include? data_type
+        raise "Not valid data_type - #{DATA_TYPES}" unless DATA_TYPES.include? data_type
 
         @values['data_type'] = data_type
       end
 
       def presentation_type(presentation_type)
         unless PRESENTATION_TYPES.include? presentation_type
-          raise 'Not valid presentation_type'
+          raise "Not valid presentation_type - #{PRESENTATION_TYPES}"
         end
 
         @values['presentation_type'] = presentation_type
@@ -40,12 +43,27 @@ module RDStation
         @values['name'] = { language.to_s => name }
       end
 
+      def validation_rules(validation_rules)
+        @values['validation_rules'] = validation_rules
+      end
+
+      def valid_options(valid_options)
+        @values['valid_options'] = valid_options
+      end
+
       def build
-        if REQUIRED_FIELDS.any? { |field| @values[field].nil? }
-          raise 'Required fields are missing'
+        empty_fields = REQUIRED_FIELDS.select { |field| @values[field].nil? }
+        unless empty_fields.empty?
+          raise "Required fields are missing - #{empty_fields}"
         end
 
         @values
+      end
+
+      private
+
+      def valid_identifier(api_identifier)
+        api_identifier.start_with? 'cf_'
       end
     end
   end
--- spec/lib/rdstation/builder/field_spec.rb
+++ spec/lib/rdstation/builder/field_spec.rb
@@ -3,31 +3,66 @@
 require 'spec_helper'
 
 RSpec.describe RDStation::Builder::Field do
-  context 'when create a builder' do
-    let(:initial_parameters) do
-      'api_identifier'
+  def valid_builder
+    described_class.new('cf_identifier')
+  end
+
+  describe 'when create a builder' do
+    context 'valid' do
+      let(:initial_parameters) do
+        'cf_api_identifier'
+      end
+
+      let(:builder) { described_class.new(initial_parameters) }
+
+      let(:expected_result) do
+        {
+          'api_identifier' => 'cf_api_identifier',
+          'data_type' => 'STRING',
+          'presentation_type' => 'TEXT_INPUT',
+          'label' => { 'pt-BR' => 'My label' },
+          'name' => { 'pt-BR' => 'My name' }
+        }
+      end
+
+      it 'returns an hash of required values' do
+        builder.label 'pt-BR', 'My label'
+        builder.name 'pt-BR', 'My name'
+        builder.data_type 'STRING'
+        builder.presentation_type 'TEXT_INPUT'
+
+        result = builder.build
+        expect(result).to eq(expected_result)
+      end
     end
+    context 'invalid' do
+      it 'using invalid api_identifier ' do
+        expect { described_class.new('identifier') }.to raise_error(
+          'api_identifier is not in a valid format, need start with "cf_"'
+        )
+      end
 
-    let(:builder) { described_class.new(initial_parameters) }
+      it 'using invalid data_type ' do
+        expect { valid_builder.data_type('type') }.to raise_error(
+          'Not valid data_type - ["STRING", "INTEGER", "BOOLEAN", "STRING[]"]'
+        )
+      end
 
-    let(:expected_result) do
-      {
-        'api_identifier' => 'api_identifier',
-        'data_type' => 'STRING',
-        'presentation_type' => 'TEXT_INPUT',
-        'label' => { 'pt-BR' => 'My label' },
-        'name' => { 'pt-BR' => 'My name' }
-      }
-    end
+      it 'using invalid presentation_type ' do
+        expect { valid_builder.presentation_type('type') }.to raise_error(
+          'Not valid presentation_type - ["TEXT_INPUT", "TEXT_AREA", "URL_INPUT", "PHONE_INPUT", "EMAIL_INPUT", "CHECK_BOX", "NUMBER_INPUT", "COMBO_BOX", "RADIO_BUTTON", "MULTIPLE_CHOICE"]'
+        )
+      end
 
-    it 'returns an hash of required values' do
-      builder.label 'pt-BR', 'My label'
-      builder.name 'pt-BR', 'My name'
-      builder.data_type 'STRING'
-      builder.presentation_type 'TEXT_INPUT'
+      it 'without api_identifier' do
+        expect { described_class.new(nil) }.to raise_error('api_identifier required')
+      end
 
-      result = builder.build
-      expect(result).to eq(expected_result)
+      it 'without required fields' do
+        expect { valid_builder.build }.to raise_error(
+          'Required fields are missing - ["data_type", "presentation_type", "label", "name"]'
+        )
+      end
     end
   end
 end
